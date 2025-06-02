5093c586657f1d669b3a0ac0b00cb9082fc86d48
shirinze <sherize1373@gmail.com>
2025-06-02 13:27:54 +0300

    Add project files.

---
+++ URLShortener/Models/ShortURL.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace URLShortener.Models;
+
+public class ShortURL
+{
+    [BsonId]
+    public string Code { get; set; } = default!;
+    public string OriginalUrl { get; set; } = default!;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+}
---
+++ URLShortener/URLShortenereDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using URLShortener.Models;
+
+namespace URLShortener;
+
+public class URLShortenereDbContext
+{
+    public IMongoCollection<ShortURL> ShortURLs { get;}
+    public URLShortenereDbContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+        var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+        ShortURLs = database.GetCollection<ShortURL>(configuration["DatabaseSettings: CollectionName"]);
+      
+    }
+}
